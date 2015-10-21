3af87891634823e6ec7ccf39848fd593e4c9d0bf
Bryan Estrito <bryan.estrito@gmail.com>
2015-10-21 16:30:26 +0800

    update tabs and panels

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
         ]
     };
 
-    $("#nav2").on('shown.bs.tab', function (e) {
+    $("#tab2").on('shown.bs.tab', function (e) {
         var ctx = document.getElementById("chart").getContext("2d");
         var chart = new Chart(ctx).Radar(data, {
             responsive : true
