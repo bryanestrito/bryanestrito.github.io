665217fe57a0a69c4bac40bd40daf5678bd45f39
Bryan Estrito <bryan.estrito@gmail.com>
2015-10-22 16:58:58 +0800

    update layout

--- js/main.js
+++ js/main.js
@@ -1,24 +1,107 @@
 $(document).ready(function () {
-    var data = {
-        labels: ["PHP", "HTML5", "CSS", "Javascript", "jQuery", "Bash", "Git"],
-        datasets: [
-            {
-                label: "My Second dataset",
-                fillColor: "rgba(151,187,205,0.2)",
-                strokeColor: "rgba(151,187,205,1)",
-                pointColor: "rgba(151,187,205,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(151,187,205,1)",
-                data: [90, 70, 80, 80, 80, 30, 90]
-            }
-        ]
+    Chart.defaults.global.responsive = true;
+
+    var osData = [
+        {
+            value: 300,
+            color:"#F7464A",
+            highlight: "#FF5A5E",
+            label: "Red"
+        },
+        {
+            value: 50,
+            color: "#46BFBD",
+            highlight: "#5AD3D1",
+            label: "Green"
+        },
+        {
+            value: 100,
+            color: "#FDB45C",
+            highlight: "#FFC870",
+            label: "Yellow"
+        }
+    ];
+
+    var langData = [
+        {
+            value: 300,
+            color:"#F7464A",
+            highlight: "#FF5A5E",
+            label: "Red"
+        },
+        {
+            value: 50,
+            color: "#46BFBD",
+            highlight: "#5AD3D1",
+            label: "Green"
+        },
+        {
+            value: 100,
+            color: "#FDB45C",
+            highlight: "#FFC870",
+            label: "Yellow"
+        }
+    ];
+
+    var fwData = [
+        {
+            value: 300,
+            color:"#F7464A",
+            highlight: "#FF5A5E",
+            label: "Red"
+        },
+        {
+            value: 50,
+            color: "#46BFBD",
+            highlight: "#5AD3D1",
+            label: "Green"
+        },
+        {
+            value: 100,
+            color: "#FDB45C",
+            highlight: "#FFC870",
+            label: "Yellow"
+        }
+    ];
+
+    var options = {
+        //Boolean - Whether we should show a stroke on each segment
+        segmentShowStroke : true,
+
+        //String - The colour of each segment stroke
+        segmentStrokeColor : "#fff",
+
+        //Number - The width of each segment stroke
+        segmentStrokeWidth : 2,
+
+        //Number - The percentage of the chart that we cut out of the middle
+        percentageInnerCutout : 80, // This is 0 for Pie charts
+
+        //Number - Amount of animation steps
+        animationSteps : 85,
+
+        //String - Animation easing effect
+        animationEasing : "easeOutBounce",
+
+        //Boolean - Whether we animate the rotation of the Doughnut
+        animateRotate : true,
+
+        //Boolean - Whether we animate scaling the Doughnut from the centre
+        animateScale : false,
+
+        //String - A legend template
+        legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
+
     };
 
     $("#tab2").on('shown.bs.tab', function (e) {
-        var ctx = document.getElementById("chart").getContext("2d");
-        var chart = new Chart(ctx).Radar(data, {
-            responsive : true
-        });
+        var os = document.getElementById("os").getContext("2d");
+        var lang = document.getElementById("lang").getContext("2d");
+        var fw = document.getElementById("fw").getContext("2d");
+
+
+        var osChart = new Chart(os).Doughnut(osData, options);
+        var langChart = new Chart(lang).Doughnut(langData);
+        var fwChart = new Chart(fw).Doughnut(fwData);
     });
 });
